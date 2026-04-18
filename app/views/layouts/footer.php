182e18eb18cc9218b34211dacba68118ceeb7d00
Jawad <jawadnasiri2222@gmail.com>
2026-04-18 11:31:02 +0200

    add footer

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,6 +1,58 @@
+<footer class="footer">
+    <div class="footer-container">
 
+        <div class="footer-brand">
+            <div class="logo">
+                <img src="logo.png" alt="Logo">
+            </div>
+            <p>Subscribe to our newsletter to get updates about our grand offers.</p>
+            <div class="social-icons">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
 
+        <div class="footer-links">
+            <h4>Quick Links</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Offer</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
 
+        <div class="footer-links">
+            <h4>Help Center</h4>
+            <ul>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Payment</a></li>
+                <li><a href="#">Returns & Refunds</a></li>
+                <li><a href="#">Checkout</a></li>
+                <li><a href="#">Delivery Information</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Subscribe to our newsletter to get updates about our grand offers.</p>
+            <div class="newsletter-input">
+                <input type="email" placeholder="Enter Your Email Here">
+                <button><i class="fa-solid fa-paper-plane"></i></button>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer-bottom">
+        <span>© 2026. All rights reserved.</span>
+        <span>Designed by <a href="#">Jawad Nasiri</a></span>
+    </div>
+</footer>
 
 </body>
 
